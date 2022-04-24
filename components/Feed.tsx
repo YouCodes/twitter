a55ec76d18d422fb46aa5a3f7dd2f747883f8f43
Louis You <81671608+YouCodes@users.noreply.github.com>
2022-04-23 20:42:45 -0700

    Fix Bug

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { SparklesIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import Input from "./Input";
@@ -7,26 +6,38 @@ import { db } from "../firebase";
 import Post from "./Post";
 import { useSession } from "next-auth/react";
 
-
 function Feed() {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-    const unsubscribe = onSnapshot(
-      query(collection(db, "posts"), orderBy("timestamp", "desc")),
-      (snapshot) => {
-        setPosts(snapshot.docs);
-      }
-    );
+  // MESSY
+  // useEffect(() => {
+  //   const unsubscribe = onSnapshot(
+  //     query(collection(db, "posts"), orderBy("timestamp", "desc")),
+  //     (snapshot) => {
+  //       setPosts(snapshot.docs);
+  //     }
+  //   );
 
-    return () => {
-      unsubscribe();
-    };
-  }, [db]);
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [db]);
+
+  // CLEAN
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
 
   return (
-    <div className="flex-grow border-l border-r border-gray-700 max-w-6xl xl:max-w-7xl 2xl:max-w-10xl sm:ml-[73px] xl:ml-[370px]">
+    <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
         <h2 className="text-lg sm:text-xl font-bold">Home</h2>
         <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
@@ -41,7 +52,7 @@ function Feed() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
