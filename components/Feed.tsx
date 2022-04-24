c665efb81985e05b7fbc5350e46ee635648805fc
Louis You <81671608+YouCodes@users.noreply.github.com>
2022-04-24 02:24:29 -0700

    Check Other Files

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -26,16 +26,16 @@ function Feed() {
   // }, [db]);
 
   // CLEAN
-  useEffect(
-    () =>
-      onSnapshot(
-        query(collection(db, "posts"), orderBy("timestamp", "desc")),
-        (snapshot) => {
-          setPosts(snapshot.docs);
-        }
-      ),
-    [db]
-  );
+  // useEffect(
+  //   () =>
+  //     onSnapshot(
+  //       query(collection(db, "posts"), orderBy("timestamp", "desc")),
+  //       (snapshot) => {
+  //         setPosts(snapshot.docs);
+  //       }
+  //     ),
+  //   [db]
+  // );
 
   return (
     <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
