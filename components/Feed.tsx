2fa254e29d580e5d1f0a6994567bc23456ccf1f6
Louis You <81671608+YouCodes@users.noreply.github.com>
2022-04-24 02:23:24 -0700

    Check Other Files

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -9,7 +9,7 @@ import { useSession } from "next-auth/react";
 
 function Feed() {
   const { data: session } = useSession();
-  const [posts, setPosts] = useState<Array>([]);
+  const [posts, setPosts] = useState([]);
 
   // MESSY
   // useEffect(() => {
@@ -47,11 +47,11 @@ function Feed() {
       </div>
 
       <Input />
-      <div className="pb-72">
+      {/* <div className="pb-72">
         {posts.map((post) => (
           <Post key={post.id} id={post.id} post={post.data()} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
